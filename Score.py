076f6dc959a8d08a2d5fcb7ec85b1d9520679fe8
Sean771 <54739639+Sean771@users.noreply.github.com>
2019-12-23 19:15:38 +0200

    jj1

---
+++ Score.py
@@ -0,0 +1,8 @@
+class Score():
+# POINTS_OF_WINNING = (DIFFICULTY X 3) + 5
+#    POINTS_OF_WINNING = 0
+#The function’s input is a variable called difficulty. The function will try to read
+#the current score in the scores file, if it fails it will create a new one and will use it to save
+#the current score.
+    def add_score(points):
+        print('aaa222')
---
+++ Utils.py
@@ -0,0 +1,16 @@
+
+# A string representing a file name. By default “Scores.txt”
+#SCORES_FILE_NAME = 'Scores.txt'
+
+#A number representing a bad return code for a function.
+#BAD_RETURN_CODE = 0
+
+# A function to clear the screen (useful when playing memory game or
+# before a new game starts).
+#def Screen_cleaner:
+#    pass
+
+#la la la
+#12345
+
+
